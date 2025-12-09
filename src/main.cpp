0902815a8de0abc2b28bc01cb3964c1f8b27eeaf
elAve2002 <nataliogrilli@hotmail.com>
2025-12-09 17:09:51 -0300

    grafo y tabla lista

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
+// main.cpp
 #include <iostream>
 #include "Grafo.h"
+#include "tabulate/table.hpp"  // Si está en include/
 
-using namespace std;
-
-
+using namespace tabulate;
 
 int main() {
-    cout << "Hello, 123!" << endl;
+    
+    // Crear tabla
+    Table table;
+    
+    table.add_row({"Nodo", "Grado", "Visitado", "Color"});
+    table.add_row({"A", "2", "Sí", "Rojo"});
+    table.add_row({"B", "3", "No", "Azul"});
+    table.add_row({"C", "1", "Sí", "Verde"});
+    
+    // Formatear
+    table.format()
+        .font_style({FontStyle::bold})
+        .border_top("═")
+        .border_bottom("═");
+    
+    std::cout << "Información del Grafo:\n";
+    std::cout << table << std::endl;
+    
     return 0;
 }
